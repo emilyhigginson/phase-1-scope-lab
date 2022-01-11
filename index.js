8ad128c34ba0cee5dc8d505ed08caca5def89cae
Emily <emilyhigginson14@gmail.com>
2022-01-11 11:11:01 -0500

    commit tree c4890495f4d71ee8cbab1b17b301348e2e52ba4f

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'emily'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'bob'
+}
